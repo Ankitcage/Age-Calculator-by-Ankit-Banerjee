a63f57a97d824e09733b023fbd2b52046600f4ce
Ankit Banerjee <95897404+Ankitcage@users.noreply.github.com>
2022-04-27 21:17:04 +0530

    Update JS_for_ ageCalculator2.js

--- JS_for_ ageCalculator2.js
+++ JS_for_ ageCalculator2.js
@@ -1,25 +1,25 @@
-function ageCalculator() {  
-    var userinput = document.getElementById("DOB").value;  
-    var dob = new Date(userinput);  
-    if(userinput==null || userinput=='') {  
-      document.getElementById("message").innerHTML = "**Choose a date please!";    
-      return false;   
-    } else {  
-      
-    //calculate month difference from current date in time  
-    var month_diff = Date.now() - dob.getTime();  
-      
-    //convert the calculated difference in date format  
-    var age_dt = new Date(month_diff);   
-      
-    //extract year from date      
-    var year = age_dt.getUTCFullYear();  
-      
-    //now calculate the age of the user  
-    var age = Math.abs(year - 1970);  
-      
-    //display the calculated age  
-    return document.getElementById("result").innerHTML =    
-             "Age is: " + age + " years. ";  
-    }  
-}  
+function ageCalculator() {
+    var userinput = document.getElementById("DOB").value;
+    var dob = new Date(userinput);
+    if(userinput==null || userinput=='') {
+      document.getElementById("message").innerHTML = "**Choose a date please!";  
+      return false; 
+    } else {
+    
+    //calculate month difference from current date in time
+    var month_diff = Date.now() - dob.getTime();
+    
+    //convert the calculated difference in date format
+    var age_dt = new Date(month_diff); 
+    
+    //extract year from date    
+    var year = age_dt.getUTCFullYear();
+    
+    //now calculate the age of the user
+    var age = Math.abs(year - 1970);
+    
+    //display the calculated age
+    return document.getElementById("result").innerHTML =  
+             "Age is: " + age + " years. ";
+    }
+}
